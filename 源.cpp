29a3ac28d46f453c871d3ec58eb65bc3cdbf87dc
wenpinglaoyao <32509896+wenpinglaoyao@users.noreply.github.com>
2018-01-18 18:32:13 +0800

    Create 源.cpp

---
+++ 源.cpp
@@ -0,0 +1,88 @@
+#include <CoreWindow.h>
+#include <tchar.h>
+#pragma comment(lib,"winmm.lib")
+#pragma comment(lib,"Msimg32.lib")
+#include "ChessGame.h"
+
+#define WINDOW_WIDTH 1308
+#define WINDOW_HEIGHT 840
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+ChessGame * game = NULL;
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+	WNDCLASSEX wndClass = { 0 }; //用WINDCLASSEX定义了一个窗口类
+	wndClass.cbSize = sizeof(WNDCLASSEX); //设置结构体的字节数大小
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;	//设置窗口的样式
+	wndClass.lpfnWndProc = WndProc;	//设置指向窗口过程函数的指针
+	wndClass.cbClsExtra = 0; //窗口类的附加内存，取0就可以了
+	wndClass.cbWndExtra = 0; //窗口的附加内存，依然取0就行了
+	wndClass.hInstance = hInstance;	 //指定包含窗口过程的程序的实例句柄。
+	wndClass.hIcon = (HICON)::LoadImage(NULL, L"icon.ico", IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE); //本地加载自定义ico图标
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW); //指定窗口类的光标句柄。
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);  //为hbrBackground成员指定一个白色画刷句柄	
+	wndClass.lpszMenuName = NULL; //设置菜单资源为空。
+	wndClass.lpszClassName = L"作者：范效萌"; //给我们的窗口命名。
+
+	if (!RegisterClassEx(&wndClass)) return -1;
+
+
+	HWND hWnd = CreateWindow(wndClass.lpszClassName, L"神魔棋，作者by范效萌",
+		WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME,//这3个表示不允许改变窗口大小
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
+
+	game = new ChessGame(hWnd); //先初始化我们的游戏类对象
+	MoveWindow(hWnd, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, true);
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	//在这里初始化游戏资源等等
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			game->timeNow = GetTickCount();
+			if (game->timeNow - game->timePre > 1) //如果时间间隔大于1毫秒，那么就调用相关函数刷新当前局势
+				game->BoardPaint();
+		}
+	}
+	ReleaseDC(hWnd, game->hDc);
+	UnregisterClass(wndClass.lpszClassName, wndClass.hInstance);
+	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_PAINT:
+		ValidateRect(hWnd, NULL);
+		break;
+	case WM_KEYDOWN: //接收键盘按下的消息并做相应处理
+		break;
+	case WM_KEYUP: //接收按键的消息并做相应处理
+		break;
+	case WM_LBUTTONDOWN:
+		int x, y;
+		x = LOWORD(lParam) / LEN;
+		y = HIWORD(lParam) / LEN;
+		if (x < GRID && y < GRID) //如果没有超出棋盘
+			game->Processmouse(x, y);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
+}
